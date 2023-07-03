544386d079a43d43ed0a87b0e79cb84510e146ae
pulkitkumarjhod <126311133+pulkitkumarjhod@users.noreply.github.com>
2023-07-03 18:16:01 +0530

    Time: 1 ms (100.00%), Space: 39.8 MB (58.89%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int mySqrt(int x) {
+//         int start = 1;
+//         int end = x;
+//         int mid =-1;
+//         while(start <= end){
+//         mid = start + (end-start)/2;
+//             if(mid *mid == x){
+//                  return mid;
+//             }else if(mid*mid < x){
+//               start = mid+1;
+            
+//              }
+//             else if ((long)mid*mid > (long)x){
+//                end = mid-1;
+//              }
+//         }
+//          return Math.round(end);
+        
+        
+        
+         if (x == 0 || x == 1)
+            return x;
+
+        int start = 1;
+        int end = x;
+        int mid = -1;
+
+        while (start <= end) {
+            // Calculate the middle point using "start + (end - start) / 2" to avoid integer overflow.
+            mid = start + (end - start) / 2;
+
+            // If the square of the middle value is greater than x, move the "end" to the left (mid - 1).
+            if ((long) mid * mid > (long) x)
+                end = mid - 1;
+            else if (mid * mid == x)
+                // If the square of the middle value is equal to x, we found the square root.
+                return mid;
+            else
+                // If the square of the middle value is less than x, move the "start" to the right (mid + 1).
+                start = mid + 1;
+        }
+
+        // The loop ends when "start" becomes greater than "end", and "end" is the integer value of the square root.
+        // However, since we might have been using integer division in the calculations,
+        // we round down the value of "end" to the nearest integer to get the correct square root.
+        return Math.round(end);
+        
+        
+        
+    }
+    
+    
+}
