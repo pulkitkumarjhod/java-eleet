4fb384067bbc39e6d6cafe7959470db87e33bada
Pulkit Kumar Jhod <126311133+pulkitkumarjhod@users.noreply.github.com>
2023-10-16 09:55:49 +0530

    Time: 207 ms (8.74%), Space: 44.8 MB (16.39%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+         for(int i=0;i<nums.length;i++){
+            int j;
+            for(j=0;j<nums.length;j++){
+                if(i!=j&&nums[i]==nums[j]){
+                    break;
+                }
+            }
+            if(j==nums.length){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
